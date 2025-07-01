35882617061884a884ad8d6b90638b710eb340a6
Niel Tran <66520542+nielpattin@users.noreply.github.com>
2025-07-01 16:09:27 +0700

    feat: add migration refresh-seed command and schedule it to run hourly

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('migrate:refresh-seed', function () {
+    $this->call('migrate:refresh', ['--seed' => true]);
+})->purpose('Refresh the database and seed it with data');
+
+Schedule::command('migrate:refresh-seed')->hourly();
